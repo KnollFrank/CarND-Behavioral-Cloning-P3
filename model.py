7197224e1be7efe9b9d40f778f49d4eaf29f774d
frankknoll <Knoll_Frank@web.de>
2018-08-13 01:25:32 +0200

    adding initial model.py from lecture

---
+++ model.py
@@ -0,0 +1,36 @@
+# https://classroom.udacity.com/nanodegrees/nd013/parts/edf28735-efc1-4b99-8fbb-ba9c432239c8/modules/6b6c37bc-13a5-47c7-88ed-eb1fce9789a0/lessons/3fc8dd70-23b3-4f49-86eb-a8707f71f8dd/concepts/b6356fc5-5191-40ae-a2d9-3c8d2c2b37bb
+import csv
+from scipy import ndimage
+import numpy as np
+
+lines = []
+with open('../CarND-Behavioral-Cloning-P3-data_from_udacity/data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines[1:]:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../CarND-Behavioral-Cloning-P3-data_from_udacity/data/IMG/' + filename
+    image = ndimage.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import  Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 7)
+model.save('model.hd5')
+
--- video.py
+++ video.py
@@ -20,10 +20,10 @@ def main():
         help='FPS (Frames per second) setting for the video.')
     args = parser.parse_args()
 
-    #convert file folder into list firltered for image file types
+    #convert file folder into list filtered for image file types
     image_list = sorted([os.path.join(args.image_folder, image_file)
                         for image_file in os.listdir(args.image_folder)])
-    
+
     image_list = [image_file for image_file in image_list if os.path.splitext(image_file)[1][1:].lower() in IMAGE_EXT]
 
     #two methods of naming output video to handle varying environemnts
@@ -32,7 +32,7 @@ def main():
 
     print("Creating video {}, FPS={}".format(args.image_folder, args.fps))
     clip = ImageSequenceClip(image_list, fps=args.fps)
-    
+
     try:
         clip.write_videofile(video_file_1)
     except:
