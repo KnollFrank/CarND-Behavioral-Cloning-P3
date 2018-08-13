152e4d8f584cfa2a9b77b3c8670acacd833a7521
frankknoll <Knoll_Frank@web.de>
2018-08-13 10:40:57 +0200

    refactoring

--- model.py
+++ model.py
@@ -1,30 +1,37 @@
 # https://classroom.udacity.com/nanodegrees/nd013/parts/edf28735-efc1-4b99-8fbb-ba9c432239c8/modules/6b6c37bc-13a5-47c7-88ed-eb1fce9789a0/lessons/3fc8dd70-23b3-4f49-86eb-a8707f71f8dd/concepts/b6356fc5-5191-40ae-a2d9-3c8d2c2b37bb
-import csv
 
 import numpy as np
+import pandas as pd
 from scipy import ndimage
 
 
 def get_driving_log():
-    def read_driving_log():
-        with open('../CarND-Behavioral-Cloning-P3-data_from_udacity/data/driving_log.csv') as csvfile:
-            reader = csv.reader(csvfile)
-            for line in reader:
-                yield line
+    def correct_path(path):
+        filename = path.split('/')[-1]
+        return '../CarND-Behavioral-Cloning-P3-data_from_udacity/data/IMG/' + filename
 
-    return list(read_driving_log())[1:]
+    df = pd.read_csv('../CarND-Behavioral-Cloning-P3-data_from_udacity/data/driving_log.csv')
+    # TODO: DRY
+    df['center'] = df['center'].map(correct_path)
+    df['left'] = df['left'].map(correct_path)
+    df['right'] = df['right'].map(correct_path)
+    return df
 
-images = []
-measurements = []
-for line in get_driving_log()[:50]:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../CarND-Behavioral-Cloning-P3-data_from_udacity/data/IMG/' + filename
-    image = ndimage.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
 
+def get_images_and_measurements(size):
+    images = []
+    measurements = []
+    df = get_driving_log()[:size]
+    for index, row in df.iterrows():
+        image = ndimage.imread(row['center'])
+        images.append(image)
+        measurement = row['steering']
+        measurements.append(measurement)
+
+    return images, measurements
+
+
+images, measurements = get_images_and_measurements(50)
 X_train = np.array(images)
 y_train = np.array(measurements)
 
