c537937df490061f62863f91e5084799b9dd6bc2
frankknoll <Knoll_Frank@web.de>
2018-08-13 11:47:31 +0200

    refactoring

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # https://classroom.udacity.com/nanodegrees/nd013/parts/edf28735-efc1-4b99-8fbb-ba9c432239c8/modules/6b6c37bc-13a5-47c7-88ed-eb1fce9789a0/lessons/3fc8dd70-23b3-4f49-86eb-a8707f71f8dd/concepts/b6356fc5-5191-40ae-a2d9-3c8d2c2b37bb
 
+import numpy as np
 import pandas as pd
 from scipy import ndimage
 
@@ -24,12 +25,12 @@ def get_driving_log():
 def get_images_and_measurements(size):
     df = get_driving_log()[:size]
     return pd.DataFrame(
-        {'image': df['center'].map(ndimage.imread).values,
+        {'image': df['center'].map(ndimage.imread).values.tolist(),
          'measurement': df['steering'].values})
 
 
 images_measurements = get_images_and_measurements(50)
-X_train = images_measurements['image'].values
+X_train = np.array(images_measurements['image'].values.tolist())
 y_train = images_measurements['measurement'].values
 
 from keras.models import Sequential
