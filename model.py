534317e81fe98a8ade1c5c0ef07b97b381469464
frankknoll <Knoll_Frank@web.de>
2018-08-14 00:56:38 +0200

    refactoring

--- model.py
+++ model.py
@@ -1,65 +1,39 @@
 # https://classroom.udacity.com/nanodegrees/nd013/parts/edf28735-efc1-4b99-8fbb-ba9c432239c8/modules/6b6c37bc-13a5-47c7-88ed-eb1fce9789a0/lessons/3fc8dd70-23b3-4f49-86eb-a8707f71f8dd/concepts/b6356fc5-5191-40ae-a2d9-3c8d2c2b37bb
 
-import numpy as np
-import pandas as pd
-from scipy import ndimage
-
-from preprocess import preprocess, create_Cropping2D, get_input_shape
+from preprocess import create_Cropping2D, get_input_shape, get_images_and_measurements, get_data
 
 
-def get_driving_log():
-    base_path = '../CarND-Behavioral-Cloning-P3-data_from_udacity/data/'
+def get_X_train_y_train():
+    print('get_images_and_measurements ...')
+    images_measurements = get_images_and_measurements(8036)
+    print('... get_images_and_measurements')
+    X_train = get_data(images_measurements, 'image')
+    y_train = get_data(images_measurements, 'measurement')
+    return X_train, y_train
 
-    def correct_path(path):
-        filename = path.split('/')[-1]
-        return base_path + 'IMG/' + filename
-
-    def correct_path_in_column(df, column):
-        df[column] = df[column].map(correct_path)
-
-    df = pd.read_csv(base_path + 'driving_log.csv')
-    correct_path_in_column(df, 'center')
-    correct_path_in_column(df, 'left')
-    correct_path_in_column(df, 'right')
-    return df
-
-
-def get_images_and_measurements(size):
-    def read_and_preprocess(image_file):
-        return preprocess(ndimage.imread(image_file))
-
-    df = get_driving_log()[:size]
-    return pd.DataFrame(
-        {'image': df['center'].map(read_and_preprocess).values,
-         'measurement': df['steering'].values})
-
-
-def get_data(df, column):
-    return np.array(df[column].values.tolist())
-
-
-print('get_images_and_measurements ...')
-images_measurements = get_images_and_measurements(8036)
-print('... get_images_and_measurements')
-X_train = get_data(images_measurements, 'image')
-y_train = get_data(images_measurements, 'measurement')
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, MaxPooling2D
 from keras.layers.convolutional import Convolution2D
 
-model = Sequential()
-model.add(Lambda(lambda image: image / 255.0 - 0.5, input_shape=get_input_shape()))
-model.add(create_Cropping2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
 
+def create_model():
+    model = Sequential()
+    model.add(Lambda(lambda image: image / 255.0 - 0.5, input_shape=get_input_shape()))
+    model.add(create_Cropping2D())
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+
+X_train, y_train = get_X_train_y_train()
+model = create_model()
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 model.save('model.hd5')
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import cv2
+import numpy as np
+import pandas as pd
 from keras.layers import Cropping2D
+from scipy import ndimage
 
 factor = 1.0 / 4.0
 
@@ -18,3 +21,34 @@ def create_Cropping2D():
 
 def get_input_shape():
     return (int(160 * factor), int(320 * factor), 3)
+
+
+def get_driving_log():
+    base_path = '../CarND-Behavioral-Cloning-P3-data_from_udacity/data/'
+
+    def correct_path(path):
+        filename = path.split('/')[-1]
+        return base_path + 'IMG/' + filename
+
+    def correct_path_in_column(df, column):
+        df[column] = df[column].map(correct_path)
+
+    df = pd.read_csv(base_path + 'driving_log.csv')
+    correct_path_in_column(df, 'center')
+    correct_path_in_column(df, 'left')
+    correct_path_in_column(df, 'right')
+    return df
+
+
+def get_images_and_measurements(size):
+    def read_and_preprocess(image_file):
+        return preprocess(ndimage.imread(image_file))
+
+    df = get_driving_log()[:size]
+    return pd.DataFrame(
+        {'image': df['center'].map(read_and_preprocess).values,
+         'measurement': df['steering'].values})
+
+
+def get_data(df, column):
+    return np.array(df[column].values.tolist())
